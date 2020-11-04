b6ead72c733b02ba374401fd01119d397a1557ac
new-biee <anhvtth1907004@fpt.edu.vn>
2020-11-04 16:09:25 +0700

    Add all file

---
+++ src/com/useoperator/Main.java
@@ -0,0 +1,22 @@
+package com.useoperator;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        float width;
+        float height;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter width: ");
+        width = scanner.nextFloat();
+
+        System.out.print("Enter height: ");
+        height = scanner.nextFloat();
+
+        float area = width * height;
+        System.out.println("Area is: " + area);
+    }
+}
+
